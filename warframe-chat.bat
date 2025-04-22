0af6072e8ea2b49bdc579c262b5fe9a44199ae03
N3M1X10 <48549372+N3M1X10@users.noreply.github.com>
2025-04-22 10:23:31 +0500

    Update warframe-chat.bat

--- warframe-chat.bat
+++ warframe-chat.bat
@@ -10,7 +10,7 @@ set "LISTS=%~dp0lists\"
 start "zapret: warframe" /min "%BIN%winws.exe" --debug=0 ^
 --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=fake --new ^
 --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=fake --dpi-desync-repeats=6 --new ^
---wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=fake,multidisorder --dpi-desync-split-pos=midsld --dpi-desync-repeats=8 --dpi-desync-fooling=md5sig, badseq --new ^
+--wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=fake,multidisorder --dpi-desync-split-pos=midsld --dpi-desync-repeats=8 --dpi-desync-fooling=md5sig,badseq --new ^
 --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" ^
 --filter-udp=443 --ipset="%LISTS%ipset-cloudflare.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
 --filter-tcp=80 --ipset="%LISTS%ipset-cloudflare.txt" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
