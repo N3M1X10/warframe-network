f2f4157017c766754d4a7c1e9868e170e6a5306d
N3M1X10 <48549372+N3M1X10@users.noreply.github.com>
2025-04-22 07:30:21 +0500

    6

--- warframe.bat
+++ warframe.bat
@@ -8,7 +8,7 @@ set "BIN=%~dp0bin\"
 set "LISTS=%~dp0lists\"
 
 start "zapret: warframe" /min "%BIN%winws.exe" --debug=0 ^
---wf-tcp=80,443,6695-6701,50666-50667 ^
+--wf-tcp=80,443,6695-6701,50000-65000 ^
 --wf-udp=443,4950-4995,50000-50100 ^
 --filter-udp=443 --ipset="%LISTS%ipset-cloudflare.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
 --filter-tcp=80 --ipset="%LISTS%ipset-cloudflare.txt" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
