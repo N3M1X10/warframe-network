22f0106ac76c8b0d11af51be566650cfb78d8148
N3M1X10 <48549372+N3M1X10@users.noreply.github.com>
2025-04-22 09:50:02 +0500

    Update warframe-chat.bat

--- warframe-chat.bat
+++ warframe-chat.bat
@@ -10,40 +10,41 @@ set "LISTS=%~dp0lists\"
 start "zapret: warframe" /min "%BIN%winws.exe" --debug=0 ^
 --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=fake
 
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=fake ^
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=fake ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=fake ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=fake ^
 
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=syndata ^
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=syndata ^
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=syndata ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=syndata ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=syndata ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=syndata ^
 
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=multisplit ^
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=multisplit ^
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=multisplit ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=multisplit ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=multisplit ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=multisplit ^
 
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop ^
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop ^
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop ^
 
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop ^
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop ^
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop ^
 
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=fake --dpi-desync-repeats=8 --dpi-desync-fooling=badseq ^
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=fake --dpi-desync-repeats=8 --dpi-desync-fooling=md5sig,badseq ^
 
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=syndata --dpi-desync-repeats=8 --dpi-desync-fooling=md5sig ^
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=syndata --dpi-desync-repeats=8 --dpi-desync-fooling=badseq ^
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=syndata --dpi-desync-repeats=8 --dpi-desync-fooling=md5sig,badseq ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=fake --dpi-desync-repeats=8 --dpi-desync-fooling=badseq ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=fake --dpi-desync-repeats=8 --dpi-desync-fooling=md5sig,badseq ^
 
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=multisplit --dpi-desync-repeats=8 --dpi-desync-fooling=md5sig ^
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=multisplit --dpi-desync-repeats=8 --dpi-desync-fooling=badseq ^
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=multisplit --dpi-desync-repeats=8 --dpi-desync-fooling=md5sig,badseq ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=syndata --dpi-desync-repeats=8 --dpi-desync-fooling=md5sig ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=syndata --dpi-desync-repeats=8 --dpi-desync-fooling=badseq ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=syndata --dpi-desync-repeats=8 --dpi-desync-fooling=md5sig,badseq ^
 
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop --dpi-desync-repeats=8 --dpi-desync-fooling=md5sig ^
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop --dpi-desync-repeats=8 --dpi-desync-fooling=badseq ^
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop --dpi-desync-repeats=8 --dpi-desync-fooling=md5sig,badseq ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=multisplit --dpi-desync-repeats=8 --dpi-desync-fooling=md5sig ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=multisplit --dpi-desync-repeats=8 --dpi-desync-fooling=badseq ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=multisplit --dpi-desync-repeats=8 --dpi-desync-fooling=md5sig,badseq ^
 
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop --dpi-desync-repeats=8 --dpi-desync-fooling=md5sig ^
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop --dpi-desync-repeats=8 --dpi-desync-fooling=badseq ^
---new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop --dpi-desync-repeats=8 --dpi-desync-fooling=md5sig,badseq ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop --dpi-desync-repeats=8 --dpi-desync-fooling=md5sig ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop --dpi-desync-repeats=8 --dpi-desync-fooling=badseq ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop --dpi-desync-repeats=8 --dpi-desync-fooling=md5sig,badseq ^
+
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop --dpi-desync-repeats=8 --dpi-desync-fooling=md5sig ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop --dpi-desync-repeats=8 --dpi-desync-fooling=badseq ^
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop --dpi-desync-repeats=8 --dpi-desync-fooling=md5sig,badseq ^
