6c83476edd301a62963ebbfc4432b3b48316bca7
N3M1X10 <48549372+N3M1X10@users.noreply.github.com>
2025-04-22 11:01:17 +0500

    1

--- warframe-chat.bat
+++ warframe-chat.bat
@@ -11,14 +11,20 @@ set autohost=%LISTS%warframe-autohost.txt
 
 start "zapret: warframe" /min "%BIN%winws.exe" --debug=0 ^
 --wf-tcp=80,443,6695-6701 --hostlist-auto="%autohost%" --ipset="%wf_ipset%" ^
--dpi-desync=fake,multidisorder --dpi-desync-split-pos=midsld --dpi-desync-repeats=8
-
-:: который еле помог, список показало - но отваливается
-rem --wf-tcp=80,443,6695-6701 --hostlist-auto="%autohost%" --ipset="%wf_ipset%" ^ 
-rem --dpi-desync=fake
+--dpi-desync=fakedsplit --dpi-desync-repeats=6 --dpi-desync-fooling=badseq
 
 rem --wf-tcp=80,443,6695-6701 --hostlist-auto="%autohost%" --ipset="%wf_ipset%" ^
 rem --dpi-desync=fake,multidisorder --dpi-desync-split-pos=midsld --dpi-desync-repeats=8 --dpi-desync-fooling=md5sig,badseq
 
 rem --wf-tcp=80,443,6695-6701 --hostlist-auto="%autohost%" --ipset="%wf_ipset%" ^
 rem --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin"
+
+
+
+:: опять еле помогло
+rem --wf-tcp=80,443,6695-6701 --hostlist-auto="%autohost%" --ipset="%wf_ipset%" ^
+rem --dpi-desync=fake,multidisorder --dpi-desync-split-pos=midsld --dpi-desync-repeats=8
+
+:: который еле помог, список показало - но отваливается
+rem --wf-tcp=80,443,6695-6701 --hostlist-auto="%autohost%" --ipset="%wf_ipset%" ^
+rem --dpi-desync=fake
