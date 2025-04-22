b33b3eeb6f64a05e8148fd1448cf6bdfc2583dfd
N3M1X10 <48549372+N3M1X10@users.noreply.github.com>
2025-04-22 09:20:58 +0500

    iana.org (1)

--- warframe-chat.bat
+++ warframe-chat.bat
@@ -8,4 +8,4 @@ set "BIN=%~dp0bin\"
 set "LISTS=%~dp0lists\"
 
 start "zapret: warframe" /min "%BIN%winws.exe" --debug=0 ^
---wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --new
+--wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%tls_clienthello_iana_org.bin" --new
