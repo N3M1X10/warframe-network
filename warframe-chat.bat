1f971d45f30e30aacea08457a999420fe80d2e7b
N3M1X10 <48549372+N3M1X10@users.noreply.github.com>
2025-04-22 09:57:40 +0500

    Update warframe-chat.bat

--- warframe-chat.bat
+++ warframe-chat.bat
@@ -10,21 +10,8 @@ set "LISTS=%~dp0lists\"
 start "zapret: warframe" /min "%BIN%winws.exe" --debug=0 ^
 --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=fake
 
-rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=fake
-rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=fake
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=syndata ^
 
-rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=syndata
-rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=syndata
-rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=syndata
+rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=multisplit ^
 
-rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=multisplit
-rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=multisplit
-rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=multisplit
-
-rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop
-rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop
-rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop
-
-rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop
-rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop
 rem --new --wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --dpi-desync=hopbyhop
