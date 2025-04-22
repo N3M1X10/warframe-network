90edc4eaacb7a437947990475f7a49771ea24da9
N3M1X10 <48549372+N3M1X10@users.noreply.github.com>
2025-04-22 09:11:41 +0500

    Update warframe-chat.bat

--- warframe-chat.bat
+++ warframe-chat.bat
@@ -7,5 +7,5 @@ cd /d "%~dp0"
 set "BIN=%~dp0bin\"
 set "LISTS=%~dp0lists\"
 
-start "zapret: warframe" /min "%BIN%winws.exe" --debug=1 ^
---filter-tcp=80,443,6695-6701 --ipset-ip=172.232.25.131 --new
+start "zapret: warframe" /min "%BIN%winws.exe" --debug=0 ^
+--wf-tcp=80,443,6695-6701 --ipset="%LISTS%\ipset-warframe.txt" --new
